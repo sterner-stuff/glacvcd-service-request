65d854dc925492a4b55cb28a97330b44506500e0
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2024-05-31 15:50:24 -0700

    Pull in default Vue2 configs, fix inline images.

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,40 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+	transpileDependencies: true,
 	css: { extract: false },
-	chainWebpack: config => {
-		if (config.plugins.has("extract-css")) {
-			const extractCSSPlugin = config.plugin("extract-css");
-			extractCSSPlugin &&
-				extractCSSPlugin.tap(() => [
-					{
-						filename: "[name].css",
-						chunkFilename: "[name].css",
-					},
-				]);
-		}
-	},
 	configureWebpack: {
 		output: {
 			filename: "js/[name].js",
 			chunkFilename: "js/[name].js",
 		},
 	},
-};
+	chainWebpack: config => {
+		config.module.rule("images").set("parser", {
+			dataUrlCondition: {
+				maxSize: Infinity,
+			},
+		});
+	},
+});
+// module.exports = {
+// 	css: { extract: false },
+// 	chainWebpack: config => {
+// 		if (config.plugins.has("extract-css")) {
+// 			const extractCSSPlugin = config.plugin("extract-css");
+// 			extractCSSPlugin &&
+// 				extractCSSPlugin.tap(() => [
+// 					{
+// 						filename: "[name].css",
+// 						chunkFilename: "[name].css",
+// 					},
+// 				]);
+// 		}
+// 		config.module.rule("images").type("asset/inline");
+// 	},
+// 	configureWebpack: {
+// 		output: {
+// 			filename: "js/[name].js",
+// 			chunkFilename: "js/[name].js",
+// 		},
+// 	},
+// };
